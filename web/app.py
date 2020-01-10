d4fb9e005bf15e168498b637e8d21dbdc58029e2
Sebuliba-Adrian <adrian.sebuliba1@gmail.com>
2020-01-10 07:30:38 +0300

    create api to count site visitors

--- web/app.py
+++ web/app.py
@@ -1,7 +1,26 @@
+from pymongo import MongoClient
 from flask import Flask
+from flask_restful import Api, Resource
 app = Flask(__name__)
+api = Api(app)
+
+client = MongoClient('mongodb://db:27017')
+db = client.aNewDB
+UserNum = db['UserNum']
+
+UserNum.insert({
+    'num_of_users': 0
+})
 
 
+class Visit(Resource):
+    def get(self):
+        prev_num = UserNum.find({})[0]['num_of_users']
+        new_num = prev_num + 1
+        UserNum.update({}, {"$set": {"num_of_users": new_num}})
+        return str("Hello user ") + str(new_num)
+
+api.add_resource(Visit, "/visit")
 @app.route('/hello')
 def index():
     return {"message": "Hello Universe!"}, 200
